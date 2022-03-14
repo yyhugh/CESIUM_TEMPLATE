418ec0f7ff9fc0dcfb52bb012847d50df3c761fc
yyhugh <hjyuanyy@gmail.com>
2022-03-15 00:33:55 +0800

    代码格式化配置（需安装vscode插件prettier，可单文件或批量格式化）

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,8 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/typescript/recommended", "plugin:prettier/recommended"],
+  plugins: ["prettier"],
   parserOptions: {
     ecmaVersion: 2020,
   },
