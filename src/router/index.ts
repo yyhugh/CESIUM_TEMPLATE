11c6bed20b5ee41ad4519d680105d1e0829918b9
yyhugh <hjyuanyy@gmail.com>
2022-03-16 14:33:10 +0800

    cesium简单示例

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 import { RouteRecordRaw } from "vue-router";
 
-const cList = ["C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11"];
+const cList = ["C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12"];
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/home",
-    name: "home",
-    component: () => import("@/views/HomeView.vue"),
-  },
+  // {
+  //   path: "/home",
+  //   name: "home",
+  //   component: () => import("@/views/HomeView.vue"),
+  // },
   ...cList.map((c) => ({
     path: `/${c}`,
     name: c,
--- src/views/C12.vue
+++ src/views/C12.vue
@@ -31,22 +31,47 @@ function init() {
     sceneModePicker: false, // 切换2D、3D地图模式控件
     baseLayerPicker: false, // 切换三维数字地球底图控件
     navigationHelpButton: false, // 帮助提示控件
-    // animation: false, // 视窗动画播放速度控件
-    // timeline: false, // 时间轴控件
+    animation: false, // 视窗动画播放速度控件
+    timeline: false, // 时间轴控件
     fullscreenButton: false, // 视窗全屏按钮控件
     imageryProvider: esri, // 加载新地图
   });
   viewerIns = viewer;
 
-  const { clock } = viewer;
+  // 加载飞机模型
+  const position = Cesium.Cartesian3.fromDegrees(116.39, 39.91, 1500);
+  const entity = viewer.entities.add({
+    position,
+    model: {
+      uri: "/static/Apps/SampleData/models/CesiumAir/Cesium_Air.glb",
+      minimumPixelSize: 128, // 模型缩放最小情况下的大小
+      maximumScale: 10000, // 模型缩放最大的比例
+      show: true, // 模型是否显示
+    },
+  });
 
-  clock.shouldAnimate = true; // 动画循环播放
-  clock.multiplier = 1000; // 时间速率
+  // 移动相机进行观测
+  viewer.trackedEntity = entity;
 
-  // 设置时间线
-  const startTime = Cesium.JulianDate.fromIso8601("2022-03-15");
-  const endTime = Cesium.JulianDate.fromIso8601("2022-03-16");
-  viewer.timeline.zoomTo(startTime, endTime);
+  // 动画循环播放
+  viewer.clock.shouldAnimate = true;
+
+  // 添加粒子特效
+  viewer.scene.primitives.add(
+    new Cesium.ParticleSystem({
+      image: "/static/Apps/SampleData/fire.png", // 粒子样式
+      imageSize: new Cesium.Cartesian2(20, 20), // 图片大小
+      startScale: 1.0, // 粒子生成时的初始大小倍数
+      endScale: 4.0, // 粒子消亡时的大小倍数
+      particleLife: 1.0, // 每个粒子存在的时间，单位s
+      speed: 5.0, // 发射粒子的速度，需要根据实际情况调试出合适值
+      emitter: new Cesium.CircleEmitter(0.5), // 发射器，粒子在该范围内随机向上发射
+      emissionRate: 5.0, // 每秒钟发射数量
+      // 粒子系统中的位置，绑定到模型实例上
+      modelMatrix: entity.computeModelMatrix(viewer.clock.startTime, new Cesium.Matrix4()),
+      lifetime: 16.0, // 指定持续时间
+    }),
+  );
 }
 
 function destroy() {
