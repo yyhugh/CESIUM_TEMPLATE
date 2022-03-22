1320fc0b2cd337b9ed8c2b1b0de49b045c4e0d8e
yyhugh <hjyuanyy@gmail.com>
2022-03-22 16:06:19 +0800

    路由配置调整

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { RouteRecordRaw } from "vue-router";
 import { rootRedirect, redirect } from "./redirect";
-import App from "@/views/App.vue";
 
 const cList = ["C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13"];
 
@@ -22,7 +21,7 @@ const routes: Array<RouteRecordRaw> = [
     meta: {
       isNav: true,
     },
-    component: App,
+    component: () => import("@/views/App.vue"),
     redirect,
     children: [
       ...cList.map((c) => ({
