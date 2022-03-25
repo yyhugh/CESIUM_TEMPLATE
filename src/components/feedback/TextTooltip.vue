153fb16b2849df9b73615213fa516a10cd5b1308
yyhugh <hjyuanyy@gmail.com>
2022-03-25 18:04:46 +0800

    TextTooltip组件更新

--- src/components/feedback/TextTooltip.vue
+++ src/components/feedback/TextTooltip.vue
@@ -1,12 +1,13 @@
 <template>
   <transition name="bounce">
-    <div class="tooltip" v-show="props.show" ref="tooltipRef"></div>
+    <div class="tooltip" v-show="props.show" ref="tooltipRef" :style="{ zIndex: props.zIndex }" @click="click"></div>
   </transition>
 </template>
 
 <script lang="ts" setup>
-import { ref, computed } from "vue";
+import { ref, computed, getCurrentInstance, defineEmits } from "vue";
 import { AnchorUtil } from "@/common/utils";
+
 const tooltipRef = ref();
 const props = defineProps({
   show: Boolean,
@@ -15,6 +16,7 @@ const props = defineProps({
   offsetX: Number,
   offsetY: Number,
   anchor: Object,
+  zIndex: Number,
 });
 const config = computed(() => ({
   width: props.width || 206,
@@ -22,21 +24,26 @@ const config = computed(() => ({
   offsetX: props.offsetX || 0,
   offsetY: props.offsetY || 0,
   anchor: props.anchor || AnchorUtil.Anchor.CENTER_BOTTOM,
+  zIndex: props.zIndex || 1,
 }));
+const vm = getCurrentInstance();
 function setPosition(origin: { x: number; y: number }) {
   const offset = AnchorUtil.getOffset(config.value.anchor as number, config.value.width, config.value.height);
   (tooltipRef.value as HTMLElement).style.top = origin.y + offset.y + config.value.offsetY + "px";
   (tooltipRef.value as HTMLElement).style.left = origin.x + offset.x + config.value.offsetX + "px";
 }
+function click() {
+  vm?.emit("click");
+}
 defineExpose({
   setPosition,
 });
+defineEmits(["click"]);
 </script>
 
 <style lang="scss" scoped>
 .tooltip {
   position: fixed;
-  z-index: 999;
   top: 0;
   left: 0;
   padding: 18px;
@@ -44,6 +51,7 @@ defineExpose({
   width: 206px;
   height: 120px;
   border-radius: 4px;
+  cursor: pointer;
   background: url("~assets/grain.png") no-repeat top right, linear-gradient(157deg, #3598f1, #4766e9);
   box-shadow: 0px 0px 12px 0px rgba(4, 7, 35, 0.2);
   &::after {
@@ -57,6 +65,9 @@ defineExpose({
     border-radius: 2px;
     transform: translate(-50%, 50%) rotate(-45deg);
     background-color: #4371eb;
+  }
+  &:hover {
+    z-index: 99999 !important;
   }
 }
 .bounce-enter-active {
