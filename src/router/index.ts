29b07acb90e761393cc24db09bbd560c039f757c
yyhugh <hjyuanyy@gmail.com>
2022-03-25 18:05:34 +0800

    cesium简单示例

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import { rootRedirect, redirect } from "./redirect";
 
 const part1 = ["C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13"];
 const part2 = ["C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11"];
+const part3 = ["C1"];
 
 /**
  * 页面路由配置
@@ -15,6 +16,24 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     redirect: rootRedirect,
+  },
+  {
+    name: "part3",
+    path: "/part3",
+    meta: {
+      isNav: true,
+    },
+    component: () => import("@/views/App.vue"),
+    redirect,
+    children: [
+      ...part3
+        .map((v) => ({
+          path: v,
+          name: `part3-${v}`,
+          component: () => import(`@/views/part3/${v}.vue`),
+        }))
+        .reverse(),
+    ],
   },
   {
     name: "part2",
